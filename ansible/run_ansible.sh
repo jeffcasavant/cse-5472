7f3cf276619d03e48f5a09a79de33941896f65a9
Jeff Casavant <jeff.casavant@gmail.com>
2015-09-29 16:15:48 -0400

    Add localhost to host configs

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # This won't work inside this bash script.  This command works on its own though.  Go figure.
-ansible-playbook -i project1-vms all.yml --ask-sudo-pass --sudo
+ansible-playbook -i inventory/project1-vms all.yml --ask-sudo-pass --sudo
